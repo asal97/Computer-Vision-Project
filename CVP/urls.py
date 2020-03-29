e9c1f77cf0eee65db9f5905fc01831b23927bf29
asal97 <asal.asgari19@gmail.com>
2020-03-29 20:39:45 +0430

    Template changed!

--- CVP/urls.py
+++ CVP/urls.py
@@ -17,11 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import dashboard
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', dashboard),
+    path('', index),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- CVP/views.py
+++ CVP/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def dashboard(request):
-    return render(request, 'dashboard.html', {})
+def index(request):
+    return render(request, 'index.html', {})
