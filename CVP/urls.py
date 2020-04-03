6d67e4722382d110c6a7bf7263dda30c55f6014e
asal97 <asal.asgari19@gmail.com>
2020-04-03 17:33:20 +0430

    index,table page,menu and few other things edited in template.

--- CVP/urls.py
+++ CVP/urls.py
@@ -17,11 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+
+from CVP import views
 from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
+    path('', views.index, name='index'),
+    path('table/', views.table, name='table')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
