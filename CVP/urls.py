510441668520ac48b3ee9680b7119dbc22298a55
themn <mt.lroc@outlook.com>
2020-04-17 16:36:04 +0430

    init form page and url

--- CVP/urls.py
+++ CVP/urls.py
@@ -16,15 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
+from django.conf.urls import url, include
 from django.conf.urls.static import static
 
-from CVP import views
-from .views import index
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('table/', views.table, name='table')
+    path('table/', views.table, name='table'),
+
+    url(r'^register/', include('registerApp.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
