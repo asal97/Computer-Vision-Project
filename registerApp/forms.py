366f716254eb9379f03b1f95bfa452e48843d1b7
themn <mt.lroc@outlook.com>
2020-04-20 01:39:18 +0430

    add widgets to form

--- registerApp/forms.py
+++ registerApp/forms.py
@@ -10,12 +10,16 @@ class PlateRegisterForm(ModelForm):
 
 
 class VehicleRegisterForm(ModelForm):
-    plate = forms.ModelChoiceField(Plate.objects.all())
-    owner = forms.ModelChoiceField(Owner.objects.all())
+    # plate = forms.ModelChoiceField(Plate.objects.all())
+    # owner = forms.ModelChoiceField(Owner.objects.all())
 
     class Meta:
         model = Vehicle
         fields = ['plate', 'type', 'color', 'owner', 'img']
         widgets = {
-            'plate': forms.Select(attrs={'class': 'form-control'}) #TODO: ADD CLASS HERE :/
+            'plate': forms.Select(choices=Plate.objects.all(), attrs={'class': 'form-control'}),
+            'owner': forms.Select(choices=Owner.objects.all(), attrs={'class': 'form-control'}),
+            'img': forms.ClearableFileInput(attrs={'class': 'form-control', 'multiple': False}),
+            'type': forms.TextInput(attrs={'class': 'form-control'}),
+            'color': forms.TextInput(attrs={'class': 'form-control'})
         }
