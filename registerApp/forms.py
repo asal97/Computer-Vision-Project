7dd620e1cdcde1d011020a4fe90f73a590f21c2b
themn <mt.lroc@outlook.com>
2020-04-20 00:02:52 +0430

    add vehicle form

--- registerApp/forms.py
+++ registerApp/forms.py
@@ -1,8 +1,21 @@
+from django import forms
 from django.forms import ModelForm
-from .models import Plate
+from .models import Plate, Vehicle, Owner
 
 
 class PlateRegisterForm(ModelForm):
     class Meta:
         model = Plate
         fields = ['plate_type', 'firstNum', 'alpha', 'secondNum', 'cityNum']
+
+
+class VehicleRegisterForm(ModelForm):
+    plate = forms.ModelChoiceField(Plate.objects.all())
+    owner = forms.ModelChoiceField(Owner.objects.all())
+
+    class Meta:
+        model = Vehicle
+        fields = ['plate', 'type', 'color', 'owner', 'img']
+        widgets = {
+            'plate': forms.Select(attrs={'class': 'form-control'}) #TODO: ADD CLASS HERE :/
+        }
--- registerApp/views.py
+++ registerApp/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
-from .forms import PlateRegisterForm
+from .forms import PlateRegisterForm, VehicleRegisterForm
 
 
 def register(request):
     plate_form = PlateRegisterForm(request.POST or None)
+    vehicle_form = VehicleRegisterForm(request.POST or None)
+
     if request.method == 'POST':
         if request.POST.get('form_name') == 'plate_register':
             plate_form.save()
+        elif request.POST.get('form_name') == 'vehicle_register':
+            vehicle_form.save()
 
     context = {
-        'plate_form': plate_form
+        'plate_form': PlateRegisterForm(),
+        'vehicle_form': VehicleRegisterForm()
     }
 
     return render(request, 'form.html', context)
