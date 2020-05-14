411b6f2f16080368a95234133d1ad2028e9a5451
asal97 <asal.asgari19@gmail.com>
2020-05-14 17:18:15 +0430

    inlined Owner and vehicle model in admin

--- registerApp/admin.py
+++ registerApp/admin.py
@@ -3,7 +3,13 @@ from django.contrib import admin
 from .models import Vehicle, Plate, Owner
 
 
+class InlineVehicle(admin.StackedInline):
+    model = Vehicle
+    extra = 1
+
+
 class OwnerAdmin(admin.ModelAdmin):
+    inlines = [InlineVehicle]
     list_display = ('first_name', 'family_name', 'nationalCode', 'phone', 'img', 'description')
     list_filter = ('nationalCode',)
 
