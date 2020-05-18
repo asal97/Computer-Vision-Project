fa355aaddb12a41d0d864853ce1682883abe139b
asal97 <asal.asgari19@gmail.com>
2020-05-18 16:01:19 +0430

    font fixed, approved button added and disabled ,shadow removed from button,jdatetime added to table

--- CVP/views.py
+++ CVP/views.py
@@ -1,6 +1,9 @@
+import datetime
+
 from django.shortcuts import render
 from registerApp.models import Plate, Owner, Vehicle
 from taradodApp.models import Taradod
+import jdatetime
 
 
 def index(request):
@@ -18,19 +21,18 @@ def table(request):
         Vehicle_list.append(vehicle.plate.get_status())
 
     for taradod in Taradod_list:
+        print("Created at %s:%s" % (taradod.seen.hour, taradod.seen.minute))
+        taradod.seen = jdatetime.datetime.fromgregorian(day=taradod.seen.day, month=taradod.seen.month.numerator,
+                                                        year=taradod.seen.year, hour=taradod.seen.hour,
+                                                        minute=taradod.seen.minute, second=taradod.seen.second,
+                                                       )
+
         if taradod.plate in Vehicle_list:
-            print("!!! ",taradod.plate)
             taradod.approved = True
-    print("!!!!!!!!!!!!!!!!!!!!!!!!")
-    print(Taradod_list)
-    print("!!!!!!!!!!!!!!!!!!!!!!!!")
-    print(Vehicle_list)
-    print("!!!!!!!!!!!!!!!!!!!!!!!!")
 
-    print(all_Vehicle_list)
-
-    return render(request, 'table.html', {
-        'Taradod_list' : Taradod_list,
+    context = {
+        'Taradod_list': Taradod_list,
         'Vehicle_list': all_Vehicle_list,
         'Owner_list': Owner_lists
-    })
+    }
+    return render(request, 'table.html', context)
