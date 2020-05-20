c7e4f329bf304dc602b52f1ea50073171a77e7d3
themn <mt.lroc@outlook.com>
2020-05-20 22:43:57 +0430

    login signup works

--- userApp/forms.py
+++ userApp/forms.py
@@ -1,5 +1,47 @@
 from django import forms
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.utils.translation import ugettext_lazy as _
+from .models import SystemUser
+
+
+class SignUpForm(UserCreationForm):
+    # first_name = forms.CharField(max_length=30, required=False, help_text='Optional.', label=u'نام')
+    # last_name = forms.CharField(max_length=30, required=False, help_text='Optional.', label=u'نام خانوادگی')
+    # email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.',
+    #                          label=u'پست الکترونیکی')
+    # phone = forms.CharField(max_length=30, required=False, label=u'شماره موبایل', widget=forms.TextInput(
+    #     attrs={'class': 'form-control', 'autocomplete': 'off', 'pattern': '[0-9]+', 'title': 'Enter numbers Only '}))
+    # password1 = forms.CharField(label=_(u"رمز عبور"),
+    #                             widget=forms.PasswordInput)
+    # password2 = forms.CharField(label=_(u"تایید رمز عبور"),
+    #                             widget=forms.PasswordInput,
+    #                             help_text=_("Enter the same password as above, for verification."))
+    # birth_date = forms.DateField(required=False, help_text="Enter your birth date.", label=u'تاریخ تولد')
+    # img = forms.ImageField(required=False)
+    phone = forms.CharField(max_length=11)
+    nationalCode = forms.CharField(max_length=10)
+
+    class Meta:
+        User._meta.get_field('email')._unique = True
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'phone', 'email', 'password1', 'password2', 'nationalCode']
+        # labels = {
+        #     'username': _(u'نام کاربری'),
+        # }
+
+
+# class SignUpForm(forms.ModelForm):
+#     class Meta:
+#         model = SystemUser
+#         fields = ['user', 'nationalCode', 'phone']
+#         widgets = {
+#             'user': forms.Select(choices=User.objects.all(), attrs={'class': 'form-control'}),
+#             # 'nationalCode': forms.ClearableFileInput(attrs={'class': 'form-control', 'multiple': False}),
+#             'phone': forms.TextInput(attrs={'class': 'form-control'}),
+#             'nationalCode': forms.TextInput(attrs={'class': 'form-control'})
+#         }
 
 
 class LoginForm(forms.Form):
--- userApp/views.py
+++ userApp/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login as auth_login
-from .forms import LoginForm
+from django.contrib.auth import login as auth_login, logout
+from .forms import LoginForm, SignUpForm
 
 
 def login(request):
-    login_form = None
+    login_form, signup_form = None, None
     if request.method == "POST":
 
         if request.POST.get('form-key') == 'ورود':
@@ -12,11 +12,28 @@ def login(request):
             if login_form.is_valid():
                 user = login_form.login(request)
                 if user:
-                    auth_login(request, user)
-                    return redirect('index')
+                    if user.profile.approved:
+                        auth_login(request, user)
+                        return redirect('index')
+
+        if request.POST.get('form-key') == 'ثبت نام':
+            signup_form = SignUpForm(request.POST)
+            if signup_form.is_valid():
+                user = signup_form.save()
+                user.refresh_from_db()
+                user.profile.birth_date = signup_form.cleaned_data.get('birth_date')
+                user.profile.phone = signup_form.cleaned_data.get('phone')
+                user.profile.nationalCode = signup_form.cleaned_data.get('nationalCode')
+                user.save()
+                # raw_password = signup_form.cleaned_data.get('password1')
+                # user = authenticate(username=user.username, password=raw_password)
+                # print(user)
+                # auth_login(request, user)
+                return redirect('logout')
 
     context = {
         'login_form': login_form,
+        'signup_form': signup_form
     }
 
     return render(request, 'login-register.html', context)
