5d03d94a1c8e392f9eaf818daef15ac4cd90364f
themn <mt.lroc@outlook.com>
2020-06-30 23:33:01 +0430

    use contrib.messages instead of status

--- userApp/views.py
+++ userApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login, logout
 from .forms import LoginForm, SignUpForm
+from django.contrib import messages
 
 
 def login(request):
@@ -19,6 +20,8 @@ def login(request):
                         if user.profile.approved:
                             auth_login(request, user)
                             return redirect('index')
+                messages.warning(request,
+                                 'نام کاربری یا رمز عبور وارد شده، در سامانه موجود نمی باشد')
 
             if request.POST.get('form-key') == 'ثبت نام':
                 signup_form = SignUpForm(request.POST)
@@ -29,18 +32,23 @@ def login(request):
                     user.profile.birth_date = signup_form.cleaned_data.get('birth_date')
                     user.profile.phone = signup_form.cleaned_data.get('phone')
                     user.profile.nationalCode = signup_form.cleaned_data.get('nationalCode')
+                    messages.success(request,
+                                     'مشخصات وارد شده با موفقیت در سامانه ثبت شد، منتظر تایید مدیر سامانه بمانید')
                     user.save()
                     # raw_password = signup_form.cleaned_data.get('password1')
                     # user = authenticate(username=user.username, password=raw_password)
                     # print(user)
                     # auth_login(request, user)
-                    status = 1
+                    # status = 1
                     return redirect('logout')
+
+        if status == -1:
+            messages.error(request,
+                           'خطا! مشخصات شما در سامانه ثبت نشد، دوباره مقادیر ورودی ها را چک نمایید')
 
         context = {
             'login_form': login_form,
             'signup_form': signup_form,
-            'status': status
         }
 
         return render(request, 'login-register.html', context)
